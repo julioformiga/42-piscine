7c62fe96cdd98caab2c8e62541b8af4284f893a9
Julio Formiga <julio.formiga@gmail.com>
2023-07-17 18:29:51 +0000

    Upd: C05/ex00 Norminette

--- c05/ex00/ft_iterative_factorial.c
+++ c05/ex00/ft_iterative_factorial.c
@@ -1,13 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julio.formiga <julio.formiga@gmail.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/17 17:44:21 by julio.formiga     #+#    #+#             */
+/*   Updated: 2023/07/17 17:44:21 by julio.formiga    ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 /* #include <stdio.h> */
 
-int ft_iterative_factorial(int nb)
+int	ft_iterative_factorial(int nb)
 {
 	int	i;
 
 	if (nb < 0)
 		return (0);
 	i = 1;
-	while(nb > 1)
+	while (nb > 1)
 	{
 		i = i * nb;
 		nb--;
