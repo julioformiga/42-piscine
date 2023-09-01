1034f3a35dbe53ee1f2661d38cb14434d6f9c31c
Julio Formiga <julio.formiga@gmail.com>
2023-09-01 18:47:15 +0200

    ft_str_is_alpha (change 0/1)

--- c02/ex02/ft_str_is_alpha.c
+++ c02/ex02/ft_str_is_alpha.c
@@ -20,8 +20,8 @@ int	ft_str_is_alpha(char *str)
 	while (str++, *str != '\0')
 		if (!(*(str) >= 'A' && *(str) <= 'Z')
 			&& !(*(str) >= 'a' && *(str) <= 'z'))
-			return (1);
-	return (0);
+			return (0);
+	return (1);
 }
 
 int	main(void)
