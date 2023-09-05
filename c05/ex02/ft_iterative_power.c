6a0b7b3652fbc3124e290e1ec192478a32eb62e0
Julio Formiga <julio.formiga@gmail.com>
2023-09-05 04:21:46 +0200

    ft_iterative_power

--- c05/ex02/ft_iterative_power.c
+++ c05/ex02/ft_iterative_power.c
@@ -9,7 +9,8 @@
 /*   Updated: 2023/07/17 17:44:21 by julio.formiga    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/* #include <stdio.h> */
+
+#include <stdio.h>
 
 int	ft_iterative_power(int nb, int power)
 {
@@ -25,20 +26,21 @@ int	ft_iterative_power(int nb, int power)
 	return (i);
 }
 
-/* int main(void) */
-/* { */
-/* 	int	i; */
-/* 	int	power = 10; */
-/*  */
-/* 	i = -1; */
-/* 	printf("Exp %d de %d: %d\n", i, power, ft_iterative_power(i, power)); */
-/* 	i = 0; */
-/* 	printf("Exp %d de %d: %d\n", i, power, ft_iterative_power(i, power)); */
-/* 	i = 2; */
-/* 	printf("Exp %d de %d: %d\n", i, power, ft_iterative_power(i, power)); */
-/* 	i = 5; */
-/* 	printf("Exp %d de %d: %d\n", i, power, ft_iterative_power(i, power)); */
-/* 	i = 10; */
-/* 	printf("Exp %d de %d: %d\n", i, power, ft_iterative_power(i, power)); */
-/* 	return (0); */
-/* } */
+int	main(void)
+{
+	int	i;
+	int	power;
+
+	power = 7;
+	i = -1;
+	printf("Exp %d de %d: %d\n", i, power, ft_iterative_power(i, power));
+	i = 0;
+	printf("Exp %d de %d: %d\n", i, power, ft_iterative_power(i, power));
+	i = 2;
+	printf("Exp %d de %d: %d\n", i, power, ft_iterative_power(i, power));
+	i = 5;
+	printf("Exp %d de %d: %d\n", i, power, ft_iterative_power(i, power));
+	i = 10;
+	printf("Exp %d de %d: %d\n", i, power, ft_iterative_power(i, power));
+	return (0);
+}
