034b48b6fe4797b139ae8c57f3b8eec3b8201ab7
Julio Formiga <julio.formiga@gmail.com>
2023-09-05 04:29:14 +0200

    ft_print_combn

--- c00/ex08/ft_print_combn.c
+++ c00/ex08/ft_print_combn.c
@@ -9,6 +9,7 @@
 /*   Updated: 2023/08/25 03:42:53 by julio.formiga    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <unistd.h>
 
 void	ft_putchar(char c)
@@ -37,7 +38,7 @@ void	ft_putnbr(int nb)
 void	ft_print_combn(int n)
 {
 	int	i;
-	int	comb[n];
+	int	comb[9];
 
 	if (n < 1 || n > 9)
 		return ;
@@ -67,6 +68,8 @@ int	main(void)
 	ft_print_combn(2);
 	write(1, "\n", 1);
 	ft_print_combn(3);
+	write(1, "\n", 1);
+	ft_print_combn(4);
 	write(1, "\n", 1);
 	return (0);
 }
