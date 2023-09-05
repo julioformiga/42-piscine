f7985dfda4c9a99a0ff00c3158fb5b12dd83017a
Julio Formiga <julio.formiga@gmail.com>
2023-09-05 04:21:31 +0200

    ft_recursive_factorial

--- c05/ex01/ft_recursive_factorial.c
+++ c05/ex01/ft_recursive_factorial.c
@@ -9,7 +9,8 @@
 /*   Updated: 2023/07/17 17:45:33 by julio.formiga    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/* #include <stdio.h> */
+
+#include <stdio.h>
 
 int	ft_recursive_factorial(int nb)
 {
@@ -20,19 +21,19 @@ int	ft_recursive_factorial(int nb)
 	return (nb * ft_recursive_factorial(nb - 1));
 }
 
-/* int main(void) */
-/* { */
-/* 	int	i; */
-/*  */
-/* 	i = -1; */
-/* 	printf("Fatorial de %d: %d\n", i, ft_recursive_factorial(i)); */
-/* 	i = 0; */
-/* 	printf("Fatorial de %d: %d\n", i, ft_recursive_factorial(i)); */
-/* 	i = 1; */
-/* 	printf("Fatorial de %d: %d\n", i, ft_recursive_factorial(i)); */
-/* 	i = 5; */
-/* 	printf("Fatorial de %d: %d\n", i, ft_recursive_factorial(i)); */
-/* 	i = 14; */
-/* 	printf("Fatorial de %d: %d\n", i, ft_recursive_factorial(i)); */
-/* 	return (0); */
-/* } */
+int	main(void)
+{
+	int	i;
+
+	i = -1;
+	printf("Fatorial de %d: %d\n", i, ft_recursive_factorial(i));
+	i = 0;
+	printf("Fatorial de %d: %d\n", i, ft_recursive_factorial(i));
+	i = 1;
+	printf("Fatorial de %d: %d\n", i, ft_recursive_factorial(i));
+	i = 5;
+	printf("Fatorial de %d: %d\n", i, ft_recursive_factorial(i));
+	i = 14;
+	printf("Fatorial de %d: %d\n", i, ft_recursive_factorial(i));
+	return (0);
+}
