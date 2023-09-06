742fa95b257010c2f94194192934a1ba9064674f
Julio Formiga <julio.formiga@gmail.com>
2023-09-06 05:53:37 +0200

    ft_strncat

--- c03/ex03/ft_strncat.c
+++ c03/ex03/ft_strncat.c
@@ -9,6 +9,7 @@
 /*   Updated: 2023/09/04 03:33:54 by julio.formiga    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
