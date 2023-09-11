dba6efac6a22f21476b46ed23521c20c75e079dc
Julio Formiga <julio.formiga@gmail.com>
2023-09-11 04:29:11 +0200

    ft_point

---
+++ c08/ex03/main.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julio.formiga <julio.formiga@gmail.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/11 04:09:09 by julio.formiga     #+#    #+#             */
+/*   Updated: 2023/09/11 04:09:44 by julio.formiga    ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_point.h"
+
+void	set_point(t_point *point)
+{
+	point->x = 42;
+	point->y = 21;
+}
+
+int	main(void)
+{
+	t_point	point;
+
+	set_point(&point);
+	return (0);
+}
